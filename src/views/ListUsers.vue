bb00952f7427341b1665a8c6b6e4e5180383ac4c
gcruzrios <gcruzr@gmail.com>
2021-05-03 11:17:55 -0600

    Crud Usuarios Completo y Ruta ListUsers Protegida

--- src/views/ListUsers.vue
+++ src/views/ListUsers.vue
@@ -10,6 +10,9 @@
 import BasicLayout from "../layouts/BasicLayout"
 import Lusers from '../components/Lusers'
 import ListUsers from '../components/ListUsers.vue'
+import { getTokenApi } from "../api/token";
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
   name: 'ListUsers',
@@ -18,7 +21,20 @@ export default {
     ListUsers,
     Lusers
     
+  },
+  setup(){
+    const router = useRouter();
+    const token = getTokenApi();
+    onMounted(() => {
+      if (!token) return router.push("/login");
+
+      else{
+         return router.push("/list-users");
+      }
+    
+    });
   }
+
 }
 </script>
 
