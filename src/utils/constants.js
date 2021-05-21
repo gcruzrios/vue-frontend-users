cf4cebefda493ec7d51a2776a3857f8d4da7674d
gcruzrios <gcruzr@gmail.com>
2021-05-20 19:23:28 -0600

    Conexion con Backend Heroku

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
+//export const API_URL = "http://localhost:9000"
 
-//export const API_URL = "https://ecommerce-strapi-vue.herokuapp.com";
-export const API_URL = "http://localhost:9000"
+export const API_URL = "https://peaceful-earth-91832.herokuapp.com"
 export const TOKEN = "token";
 export const PRODUCTS = "products";
